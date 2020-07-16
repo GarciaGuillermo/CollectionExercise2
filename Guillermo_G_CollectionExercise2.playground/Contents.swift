20e6080394a90fc99bd9bc6c8ab1a6bfc7bb4099
GarciaGuillermo <67976128+GarciaGuillermo@users.noreply.github.com>
2020-07-16 14:21:14 -0500

    Add files via upload

---
+++ Guillermo_G_CollectionExercise2.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+var videoGameCases = ["Infinite Warfare" , "Modern Warefare" , "Modern Warfare Remastered" ,  "Modern Warfare 3" , "Grand Theft Auto 5" , "Battlefield One" , "Batman Arkham City" , "Bioshock Series" , "Mincraft" , "Oblivion" , "Injustice"]
+videoGameCases.sort()
+print(videoGameCases)
+
+
